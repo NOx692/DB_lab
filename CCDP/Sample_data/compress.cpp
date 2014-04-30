2491b1f3d735be3464ad873555142985792e8b13
NOx <nox0715@gmail.com>
2014-04-30 20:31:54 +0900

    Commit message

--- CCDP/Sample_data/compress.cpp
+++ CCDP/Sample_data/compress.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 const int C = 30; // C is after compressed length
+const int MAX = 1000;
 
 struct RGB{
   int r, g, b;
@@ -10,15 +11,17 @@ struct RGB{
 
 int H, W;
 int L;
-vector<RGB> V;
+vector<RGB> V[MAX];
 
 void input(){
   cin >> H >> W;
   L = H * W;
   int r, g, b;
-  for(int i = 0 ; i < L ; i++){
-    scanf("%d %d %d", &r, &g, &b);
-    V.push_back(RGB(r, g, b));
+  for(int i = 0 ; i < H ; i++){
+    for(int j = 0 ; j < W ; j++){
+      scanf("%d %d %d", &r, &g, &b);
+      V[i].push_back(RGB(r, g, b));
+    }
   }
   cout << H << ' ' << W << endl;
 }
@@ -29,6 +32,33 @@ void compress(){
   CW = W / C + (W % C != 0);
   
   cout << CH << ' ' << CW << endl;
+  
+  vector<RGB> res;
+
+  for(int i = 0 ; i < H - CH ; i += CH){
+    for(int j = 0 ; j < W - CW ; j += CW){
+      RGB ave(0, 0, 0);
+      for(int a = i ; a < i + CH ; a++){
+	for(int b = j ; b < j + CW ; b++){
+	  cout << a << ' ' << b << endl;	  
+	  ave.r += V[a][b].r;
+	  ave.g += V[a][b].g;
+	  ave.b += V[a][b].b;
+	}
+      }
+      cout << endl;
+      ave.r /= (CH * CW);
+      ave.g /= (CH * CW);
+      ave.b /= (CH * CW);
+      res.push_back(ave);
+    }
+  }  
+  
+  cout << (int)res.size() << endl;
+  
+  for(int i = 0 ; i < (int)res.size() ; i++){
+    cout << res[i].r << ' ' << res[i].g << ' ' << res[i].b << endl;
+  }
 }
 
 int main(){
